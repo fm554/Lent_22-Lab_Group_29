4ff932645f5d60d5bb9c7d47aece839f3fe77e58
Gus Molyneux <fm554@cam.ac.uk>
2022-02-07 13:08:53 +0000

    B test fix 2

--- test_geo.py
+++ test_geo.py
@@ -7,9 +7,9 @@ def test_stations_by_distance():
     # Build list of stations
     stations = build_station_list()
     #generates full stations by distance list
-    station_distances = sorted(stations_by_distance(stations, (52.2053, 0.1218)))
+    station_distances = stations_by_distance(stations, (52.2053, 0.1218))
 
-    assert round(station_distances[0][1], 2) == 0.84
+    assert round(station_distances[0][2], 2) == 0.84
 
 #1C test
 
